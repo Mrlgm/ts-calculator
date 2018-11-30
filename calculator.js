12dfaaff338885a25b8590b3ada4c4516204d5db
Mrlgm <877205619@qq.com>
2018-11-30 23:56:26 +0800

    完成加减乘除

--- calculator.js
+++ calculator.js
@@ -1,51 +1,100 @@
 var Calculator = /** @class */ (function () {
     function Calculator() {
+        this.keys = [
+            ["Clear", "÷"],
+            ["7", "8", "9", "×"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "+"],
+            ["0", ".", "="]
+        ];
+        this.createContainer();
+        this.createOutpput();
+        this.createButtons();
+        this.bindEvents();
     }
-    Calculator.prototype.createButton = function () { };
-    Calculator.prototype.createButtons = function () { };
-    Calculator.prototype.createContainer = function () { };
-    Calculator.prototype.createOutpput = function () { };
-    Calculator.prototype.bindEvents = function () { };
+    Calculator.prototype.createButton = function (text, container, className) {
+        var button = document.createElement('button');
+        button.textContent = text;
+        if (className) {
+            button.className = className;
+        }
+        container.appendChild(button);
+    };
+    Calculator.prototype.createButtons = function () {
+        var _this = this;
+        this.keys.forEach(function (keysList) {
+            var div = document.createElement('div');
+            keysList.forEach(function (key) {
+                _this.createButton(key, div, "button text-" + key);
+            });
+            _this.container.appendChild(div);
+        });
+    };
+    Calculator.prototype.createContainer = function () {
+        var container = document.createElement('div');
+        container.classList.add('calculator');
+        document.body.appendChild(container);
+        this.container = container;
+    };
+    Calculator.prototype.createOutpput = function () {
+        var output = document.createElement('div');
+        output.classList.add('output');
+        var span = document.createElement('span');
+        span.textContent = '0';
+        this.span = span;
+        output.appendChild(span);
+        this.output = output;
+        this.container.appendChild(output);
+    };
+    Calculator.prototype.bindEvents = function () {
+        var _this = this;
+        this.container.addEventListener('click', function (event) {
+            if (event.target instanceof HTMLButtonElement) {
+                var text = event.target.textContent;
+                if ('0123456789'.indexOf(text) >= 0) {
+                    if (_this.operator) {
+                        if (_this.n2) {
+                            _this.n2 = parseInt(_this.n2.toString() + text);
+                        }
+                        else {
+                            _this.n2 = parseInt(text);
+                        }
+                        _this.span.textContent = _this.n2.toString();
+                    }
+                    else {
+                        if (_this.n1) {
+                            _this.n1 = parseInt(_this.n1.toString() + text);
+                        }
+                        else {
+                            _this.n1 = parseInt(text);
+                        }
+                        _this.span.textContent = _this.n1.toString();
+                    }
+                }
+                else if ('+-×÷'.indexOf(text) >= 0) {
+                    _this.operator = text;
+                }
+                else if ('='.indexOf(text) >= 0) {
+                    var result = void 0;
+                    if (_this.operator === '+') {
+                        result = _this.n1 + _this.n2;
+                    }
+                    else if (_this.operator === '-') {
+                        result = _this.n1 - _this.n2;
+                    }
+                    else if (_this.operator === '×') {
+                        result = _this.n1 * _this.n2;
+                    }
+                    else if (_this.operator === '÷') {
+                        result = _this.n1 / _this.n2;
+                    }
+                    _this.span.textContent = result.toString();
+                }
+            }
+        });
+    };
+    Calculator.prototype.x = function () {
+    };
     return Calculator;
 }());
-function createButton(text, container, className) {
-    var button = document.createElement('button');
-    button.textContent = text;
-    if (className) {
-        button.className = className;
-    }
-    container.appendChild(button);
-}
-var container = document.createElement('div');
-container.classList.add('calculator');
-document.body.appendChild(container);
-var output = document.createElement('div');
-output.classList.add('output');
-var span = document.createElement('span');
-span.textContent = '0';
-output.appendChild(span);
-container.appendChild(output);
-var n1;
-var n2;
-var operator;
-container.addEventListener('click', function (event) {
-    if (event.target instanceof HTMLButtonElement) {
-        var text = event.target.textContent;
-        if ('0123456789'.indexOf(text) >= 0) {
-        }
-    }
-});
-var keys = [
-    ["Clear", "÷"],
-    ["7", "8", "9", "×"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="]
-];
-keys.forEach(function (keysList) {
-    var div = document.createElement('div');
-    keysList.forEach(function (key) {
-        createButton(key, div, "button text-" + key);
-    });
-    container.appendChild(div);
-});
+new Calculator();
--- calculator.ts
+++ calculator.ts
@@ -1,59 +1,95 @@
 
-class Calculator{
-  public container:HTMLDivElement;
-  private output:HTMLDivElement;
-  private span:HTMLSpanElement
-  public keys
-  constructor(){}
-  createButton(){}
-  createButtons(){}
-  createContainer(){}
-  createOutpput(){}
-  bindEvents(){}
-}
-
-function createButton(text: string, container: HTMLElement, className: string) {
-  let button: HTMLButtonElement = document.createElement('button')
-  button.textContent = text
-  if (className) { button.className = className }
-  container.appendChild(button)
-}
-
-let container = document.createElement('div')
-container.classList.add('calculator')
-document.body.appendChild(container)
-
-let output: HTMLDivElement = document.createElement('div')
-output.classList.add('output')
-let span: HTMLSpanElement = document.createElement('span')
-span.textContent = '0'
-output.appendChild(span)
-container.appendChild(output)
-
-let n1:number
-let n2:number
-let operator:string
-
-container.addEventListener('click', function (event) {
-  if(event.target instanceof HTMLButtonElement){
-    let text = event.target.textContent
-    if('0123456789'.indexOf(text)>=0){
-
-    }
+class Calculator {
+  public container: HTMLDivElement;
+  private output: HTMLDivElement;
+  private span: HTMLSpanElement
+  public n1: number
+  public n2: number
+  public operator: string
+  public keys: Array<Array<string>> = [
+    ["Clear", "÷"],
+    ["7", "8", "9", "×"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="]]
+  constructor() {
+    this.createContainer()
+    this.createOutpput()
+    this.createButtons()
+    this.bindEvents()
   }
-})
+  createButton(text: string, container: HTMLElement, className: string) {
+    let button: HTMLButtonElement = document.createElement('button')
+    button.textContent = text
+    if (className) { button.className = className }
+    container.appendChild(button)
+  }
+  createButtons() {
+    this.keys.forEach((keysList: Array<string>) => {
+      let div: HTMLDivElement = document.createElement('div')
+      keysList.forEach((key: string) => {
+        this.createButton(key, div, `button text-${key}`)
+      })
+      this.container.appendChild(div)
+    })
+  }
+  createContainer() {
+    let container = document.createElement('div')
+    container.classList.add('calculator')
+    document.body.appendChild(container)
+    this.container = container
+  }
+  createOutpput() {
+    let output: HTMLDivElement = document.createElement('div')
+    output.classList.add('output')
+    let span: HTMLSpanElement = document.createElement('span')
+    span.textContent = '0'
+    this.span =  span
+    output.appendChild(span)
+    this.output = output
+    this.container.appendChild(output)
+  }
+  bindEvents() {
+    this.container.addEventListener('click', (event) => {
+      if (event.target instanceof HTMLButtonElement) {
+        let text = event.target.textContent
+        if ('0123456789'.indexOf(text) >= 0) {
+          if (this.operator) {
+            if (this.n2) {
+              this.n2 = parseInt(this.n2.toString() + text)
+            } else {
+              this.n2 = parseInt(text)
+            }
+            this.span.textContent = this.n2.toString()
+          } else {
+            if (this.n1) {
+              this.n1 = parseInt(this.n1.toString() + text)
+            } else {
+              this.n1 = parseInt(text)
+            }
+            this.span.textContent = this.n1.toString()
+          }
+        } else if ('+-×÷'.indexOf(text) >= 0) {
+          this.operator = text
+        } else if ('='.indexOf(text) >= 0) {
+          let result;
+          if (this.operator === '+') {
+            result = this.n1 + this.n2
+          }else if (this.operator === '-'){
+            result = this.n1 - this.n2
+          }else if(this.operator === '×'){
+            result = this.n1*this.n2
+          }else if(this.operator === '÷'){
+            result = this.n1/this.n2
+          }
+          this.span.textContent = result.toString()
+        }
+      }
+    })
+  }
+  x() {
 
-let keys: Array<Array<string>> = [
-  ["Clear", "÷"],
-  ["7", "8", "9", "×"],
-  ["4", "5", "6", "-"],
-  ["1", "2", "3", "+"],
-  ["0", ".", "="]]
+  }
+}
 
-keys.forEach((keysList: Array<string>) => {
-  let div: HTMLDivElement = document.createElement('div')
-  keysList.forEach((key: string) => {
-    createButton(key, div, `button text-${key}`)
-  })
-  container.appendChild(div)
-})
+new Calculator()
